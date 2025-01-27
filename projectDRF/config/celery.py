f20a14a85e65fb80a84ca6aa6a16a931edfedd69
Nikolay_Lyalkin <serega94nn@yandex.ru>
2025-01-27 19:42:16 +0300

    celery error

--- projectDRF/config/celery.py
+++ projectDRF/config/celery.py
@@ -2,7 +2,6 @@ import os
 
 from celery import Celery
 
-
 # Установка переменной окружения для настроек проекта
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -15,4 +14,3 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
-
